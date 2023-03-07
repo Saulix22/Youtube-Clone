171109140372467a6dd15f40740921d8686debe2
Saulix22 <saul.castaneda.dev@gmail.com>
2023-03-06 19:09:44 -0700

    Login and signup views and urls added

--- Youtube/urls.py
+++ Youtube/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio')
+    path('', views.inicio, name='inicio'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
--- Youtube/views.py
+++ Youtube/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return render(request, 'pages/home.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def signup(request):
+    return render(request, 'pages/signup.html')
